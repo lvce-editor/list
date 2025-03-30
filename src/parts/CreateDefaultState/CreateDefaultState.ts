348b22c5339746ae2d730d8b295740f1955d27f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 22:18:16 +0200

    feature: add createDefaultState function (#2)

---
+++ src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -0,0 +1,22 @@
+import type { List } from '../List/List.ts'
+
+export const createDefaultState = (): List<string> => {
+  const initialItems: readonly string[] = []
+  const initialState: List<string> = {
+    focusedIndex: -1,
+    items: initialItems,
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    maxLineY: 10,
+    minLineY: 0,
+    deltaY: 0,
+    finalDeltaY: 0,
+    handleOffset: 0,
+    scrollBarActive: false,
+    scrollBarHeight: 0,
+    x: 0,
+    y: 0,
+  }
+  return initialState
+}
--- test/ListFocusFirst.test.ts
+++ test/ListFocusFirst.test.ts
@@ -1,24 +1,12 @@
 import { expect, test } from '@jest/globals'
 import type { List } from '../src/parts/List/List.ts'
 import * as ListFocusFirst from '../src/parts/ListFocusFirst/ListFocusFirst.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
 test('focusFirst', () => {
-  const initialItems: readonly string[] = []
   const initialState: List<string> = {
+    ...createDefaultState(),
     focusedIndex: -1,
-    items: initialItems,
-    headerHeight: 0,
-    height: 100,
-    itemHeight: 20,
-    maxLineY: 10,
-    minLineY: 0,
-    deltaY: 0,
-    finalDeltaY: 0,
-    handleOffset: 0,
-    scrollBarActive: false,
-    scrollBarHeight: 0,
-    x: 0,
-    y: 0,
   }
   const result = ListFocusFirst.focusFirst(initialState)
   expect(result.focusedIndex).toBe(-1)
