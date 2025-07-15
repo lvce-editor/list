13f9a8020b902d93c09c9f8a8e235776feb601b0
Le Vivilet <levivlet@pm.me>
2025-07-15 10:47:40 +0200

    simplify code

--- src/parts/ListFocusIndexScrollDown/ListFocusIndexScrollDown.ts
+++ src/parts/ListFocusIndexScrollDown/ListFocusIndexScrollDown.ts
@@ -9,6 +9,7 @@ export const focusIndexScrollDown = <T, State extends List<T>>(
   itemHeight: number,
   itemsLength: number,
 ): State => {
+  const { deltaY, finalDeltaY, height, headerHeight, scrollBarHeight } = state
   const newMaxLineY = Math.min(index + 1, itemsLength)
   const fittingItems = GetNumberOfVisibleItems.getNumberOfVisibleItems(
     listHeight,
@@ -21,9 +22,9 @@ export const focusIndexScrollDown = <T, State extends List<T>>(
       : newMinLineY * itemHeight - (listHeight % itemHeight) + itemHeight
   const scrollBarY = getScrollBarY(
     newDeltaY,
-    state.finalDeltaY,
-    state.height - state.headerHeight,
-    state.scrollBarHeight,
+    finalDeltaY,
+    height - headerHeight,
+    scrollBarHeight,
   )
   return {
     ...state,
