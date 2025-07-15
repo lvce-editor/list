b96f6db60db81b9029565f889115f63d3da1c44c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-15 10:45:32 +0200

    feature: format code (#22)

--- src/parts/ListFocusNextPage/ListFocusNextPage.ts
+++ src/parts/ListFocusNextPage/ListFocusNextPage.ts
@@ -2,11 +2,16 @@ import type { List } from '../List/List.ts'
 import * as Arrays from '../Arrays/Arrays.ts'
 import { focusIndex } from '../ListFocusIndex/ListFocusIndex.ts'
 
-export const focusNextPage = <T, State extends List<T>>(state: State): State => {
+export const focusNextPage = <T, State extends List<T>>(
+  state: State,
+): State => {
   const { focusedIndex, items, maxLineY, minLineY } = state
   if (Arrays.isLastIndex(items, focusedIndex)) {
     return state
   }
-  const indexNextPage = Math.min(maxLineY + (maxLineY - minLineY) - 2, Arrays.lastIndex(items))
+  const indexNextPage = Math.min(
+    maxLineY + (maxLineY - minLineY) - 2,
+    Arrays.lastIndex(items),
+  )
   return focusIndex(state, indexNextPage)
 }
